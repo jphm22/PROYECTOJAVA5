a2317b447f5856c00c7ffe68ca35da21c183918f
jphm22 <jphuayre2020@gmail.com>
2024-12-04 08:58:11 -0500

    Aspectos del formulario

--- Backend/demo_utp_1/src/main/java/com/utp_example_1/demo_utp_1/interfaces/EmpleadoRepository.java
+++ Backend/demo_utp_1/src/main/java/com/utp_example_1/demo_utp_1/interfaces/EmpleadoRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
     Empleado findByCorreoAndContraseña(String correo, String contraseña);
+    /*Empleado findByCorreo(String correo);*/
 }
 
